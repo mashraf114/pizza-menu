1a3b03a5de5a9deb0edcd6360d28cf54743f06b0
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2026-06-10 06:48:10 +0300

    feat: render elements list using map

--- src/index.js
+++ src/index.js
@@ -24,43 +24,61 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu:</h2>
-      <Pizza />
-      <Pizza />
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza
+            pizzaObj={pizza}
+            key={pizza.name}
+          />
+        ))}
+      </ul>
+
+      {/* <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and
+        ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      /> */}
     </main>
   );
 }
 
+function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img
+        src={props.pizzaObj.photoName}
+        alt={props.pizzaObj.name}
+      ></img>
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>
+          {props.pizzaObj.ingredients}
+        </p>
+        <span>
+          {props.pizzaObj.price + 3}
+        </span>
+      </div>
+    </li>
+  );
+}
+
 function Footer() {
-  const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
-  const isOpen =
-    hour >= openHour &&
-    hour <= closeHour;
+  // const hour = new Date().getHours();
+  // const openHour = 12;
+  // const closeHour = 22;
+  // const isOpen =
+  //   hour >= openHour &&
+  //   hour <= closeHour;
   // ? alert("Currently open")
   // : alert("Currently close");
-  console.log(isOpen);
+  // console.log(isOpen);
   return (
     <footer className="footer">
       {new Date().toLocaleTimeString()}
       We're currently open!
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img
-        src="pizzas/spinaci.jpg"
-        alt="Spinaci"
-      ></img>
-      <h3>Pizza Spinaci</h3>
-      <p>
-        Tomato, mozarella, spinach, and
-        ricotta cheese
-      </p>
-    </div>
   );
 }
 
@@ -73,52 +91,52 @@ root.render(
   </React.StrictMode>,
 );
 
-// const pizzaData = [
-//   {
-//     name: "Focaccia",
-//     ingredients:
-//       "Bread with italian olive oil and rosemary",
-//     price: 6,
-//     photoName: "pizzas/focaccia.jpg",
-//     soldOut: false,
-//   },
-//   {
-//     name: "Pizza Margherita",
-//     ingredients: "Tomato and mozarella",
-//     price: 10,
-//     photoName: "pizzas/margherita.jpg",
-//     soldOut: false,
-//   },
-//   {
-//     name: "Pizza Spinaci",
-//     ingredients:
-//       "Tomato, mozarella, spinach, and ricotta cheese",
-//     price: 12,
-//     photoName: "pizzas/spinaci.jpg",
-//     soldOut: false,
-//   },
-//   {
-//     name: "Pizza Funghi",
-//     ingredients:
-//       "Tomato, mozarella, mushrooms, and onion",
-//     price: 12,
-//     photoName: "pizzas/funghi.jpg",
-//     soldOut: false,
-//   },
-//   {
-//     name: "Pizza Salamino",
-//     ingredients:
-//       "Tomato, mozarella, and pepperoni",
-//     price: 15,
-//     photoName: "pizzas/salamino.jpg",
-//     soldOut: true,
-//   },
-//   {
-//     name: "Pizza Prosciutto",
-//     ingredients:
-//       "Tomato, mozarella, ham, aragula, and burrata cheese",
-//     price: 18,
-//     photoName: "pizzas/prosciutto.jpg",
-//     soldOut: false,
-//   },
-// ];
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients:
+      "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients:
+      "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients:
+      "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients:
+      "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients:
+      "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
